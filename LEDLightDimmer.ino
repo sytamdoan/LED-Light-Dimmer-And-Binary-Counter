f35d64114c4c79332558e33d29cacb3eaba302cf
sytamdoan <48368269+sytamdoan@users.noreply.github.com>
2020-06-03 23:27:49 -0700

    Updated Code
    More comments and better code practices

--- LEDLightDimmer.ino
+++ LEDLightDimmer.ino
@@ -1,12 +1,12 @@
-int digit0 = 11;
+int digit0 = 11; /* digit0,digit1,digit2,digit3 are all integers referring to the pin which will be used to light up the LED */
 int digit1 = 10;
 int digit2 = 6;
 int digit3 = 5;
-int timeDelay = 300;
-int sensor = A0;
-int reading = analogRead(sensor);
-int brightness = map(reading,0,1023,0,255);
-int wait = 0;
+int timeDelay = 300; /*Time delay is the amount of time wait, in millisecond, before the LED can count up*/
+int sensor = A0; /*sensor indicates which analog pin is accepting the potentiometer and reading its value*/
+int reading = analogRead(sensor); /*Reads the value from the potentiometer*/
+int brightness = map(reading,0,1023,0,255); /* Maps the value of the potentiometer linearly from 0-255 */
+int wait = 0; /* The integer used to count up while the LED lights are in it wait period */
 /*
  Author: Tam Doan
  Description: This project counts binary from 0 to 15 with LED lights. In addition, a potentiometer is also used to dim the lights as
@@ -26,188 +26,170 @@ void setup() {
 void loop() {
 
  /*
- Each of these while loops waits 500 millisecond until the next binary number is displayed on the LED.
+ Each of these for loops waits 500 millisecond until the next binary number is displayed on the LED.
  During this wait time, the code checks the the value of the potentiometer and map it into a value of 0-255. This value then determines the brightness
- of each LED. This while loop can also be replaced with a for loop, which would probably be better.
+ of each LED. 
  */
  
-  while(wait < timeDelay) {
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     digitalWrite(digit0,LOW);
     digitalWrite(digit1,LOW);
     digitalWrite(digit2,LOW);
     digitalWrite(digit3,LOW);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
 
-  while(wait < timeDelay) {
+
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     analogWrite(digit0,brightness);
     digitalWrite(digit1,LOW);
     digitalWrite(digit2,LOW);
     digitalWrite(digit3,LOW);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
 
 
-  while(wait < timeDelay) {
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     digitalWrite(digit0,LOW);
     analogWrite(digit1,brightness);
     digitalWrite(digit2,LOW);
     digitalWrite(digit3,LOW);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
+
  
-  while(wait < timeDelay) {
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     analogWrite(digit0,brightness);
     analogWrite(digit1,brightness);
     digitalWrite(digit2,LOW);
     digitalWrite(digit3,LOW);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
 
-  while(wait < timeDelay) {
+
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     digitalWrite(digit0,LOW);
     digitalWrite(digit1,LOW);
     analogWrite(digit2,brightness);
     digitalWrite(digit3,LOW);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
+
   
-  while(wait < timeDelay) {
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     analogWrite(digit0,brightness);
     digitalWrite(digit1,LOW);
     analogWrite(digit2,brightness);
     digitalWrite(digit3,LOW);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
 
-  while(wait < timeDelay) {
+
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     digitalWrite(digit0,LOW);
     analogWrite(digit1,brightness);
     analogWrite(digit2,brightness);
     digitalWrite(digit3,LOW);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
 
-  while(wait < timeDelay) {
+
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     analogWrite(digit0,brightness);
     analogWrite(digit1,brightness);
     analogWrite(digit2,brightness);
     digitalWrite(digit3,LOW);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
   
-  while(wait < timeDelay) {
-    takeAnalog();
-    digitalWrite(digit0,LOW);
-    digitalWrite(digit1,LOW);
-    digitalWrite(digit2,LOW);
-    analogWrite(digit3,brightness);
-    delay(1);
-    wait = wait + 1;
-  }
-  wait = 0;
-
-  while(wait < timeDelay) {
-    takeAnalog();
-    analogWrite(digit0,brightness);
-    digitalWrite(digit1,LOW);
-    digitalWrite(digit2,LOW);
-    analogWrite(digit3,brightness);
-    delay(1);
-    wait = wait + 1;
-  }
-  wait = 0;
-
-  while(wait < timeDelay) {
-    takeAnalog();
-    digitalWrite(digit0,LOW);
-    analogWrite(digit1,brightness);
-    digitalWrite(digit2,LOW);
-    analogWrite(digit3,brightness);
-    delay(1);
-    wait = wait + 1;
-  }
-  wait = 0;
-
-  while(wait < timeDelay) {
-    takeAnalog();
-    analogWrite(digit0,brightness);
-    analogWrite(digit1,brightness);
-    digitalWrite(digit2,LOW);
-    analogWrite(digit3,brightness);
-    delay(1);
-    wait = wait + 1;
-  }
-  wait = 0;
   
-  while(wait < timeDelay) {
+  for(wait = 0;wait < timeDelay; wait++) {
+    takeAnalog();
+    digitalWrite(digit0,LOW);
+    digitalWrite(digit1,LOW);
+    digitalWrite(digit2,LOW);
+    analogWrite(digit3,brightness);
+    delay(1);
+  }
+
+
+  for(wait = 0;wait < timeDelay; wait++) {
+    takeAnalog();
+    analogWrite(digit0,brightness);
+    digitalWrite(digit1,LOW);
+    digitalWrite(digit2,LOW);
+    analogWrite(digit3,brightness);
+    delay(1);
+  }
+
+
+  for(wait = 0;wait < timeDelay; wait++) {
+    takeAnalog();
+    digitalWrite(digit0,LOW);
+    analogWrite(digit1,brightness);
+    digitalWrite(digit2,LOW);
+    analogWrite(digit3,brightness);
+    delay(1);
+  }
+
+
+  for(wait = 0;wait < timeDelay; wait++) {
+    takeAnalog();
+    analogWrite(digit0,brightness);
+    analogWrite(digit1,brightness);
+    digitalWrite(digit2,LOW);
+    analogWrite(digit3,brightness);
+    delay(1);
+  }
+
+  
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     digitalWrite(digit0,LOW);
     digitalWrite(digit1,LOW);
     analogWrite(digit2,brightness);
     analogWrite(digit3,brightness);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
 
-  while(wait < timeDelay) {
+
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     analogWrite(digit0,brightness);
     digitalWrite(digit1,LOW);
     analogWrite(digit2,brightness);
     analogWrite(digit3,brightness);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
 
-  while(wait < timeDelay) {
+
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     digitalWrite(digit0,LOW);
     analogWrite(digit1,brightness);
     analogWrite(digit2,brightness);
     analogWrite(digit3,brightness);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
   
 
-  while(wait < timeDelay) {
+  for(wait = 0;wait < timeDelay; wait++) {
     takeAnalog();
     analogWrite(digit0,brightness);
     analogWrite(digit1,brightness);
     analogWrite(digit2,brightness);
     analogWrite(digit3,brightness);
     delay(1);
-    wait = wait + 1;
   }
-  wait = 0;
+
 
 
 }
